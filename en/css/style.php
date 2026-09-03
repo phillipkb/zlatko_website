baccc8e303b9e04287b136ae789798b1608c9335
phillip bannister <phillip.kbannister@gmail.com>
2026-09-03 12:34:54 -0700

    init

---
+++ en/css/style.php
@@ -0,0 +1,271 @@
+  
+
+/* A Free Design by Bryant Smith (bryantsmith.com) */
+
+
+
+body {
+	margin: 0;
+	padding: 0;
+	text-align: left;
+	font-family:"Adobe Garamond Pro Bold", Georgia, "Times New Roman", Times, serif;
+	font-size: 13px;
+	color: #000000;
+	background: #000000 url(http://antipropaganda.net/en/img/background.png);
+	background-repeat:repeat-x;
+}
+*
+{
+  margin: 0 auto 0 auto;
+ text-align:left;}
+
+#page
+{
+  display: block; 
+  height:auto;
+  position: relative; 
+  overflow: hidden; 
+  width: 670px;
+}
+
+.topNaviagationLink
+{
+text-align:center;
+position:relative;
+margin-top:30px;
+font-size:16px;
+margin-left:-10px;
+width:121px;
+height: 35px;
+line-height: 35px;
+float:left;
+color:#000000;
+font-family:"Adobe Garamond Pro Bold", Garamond, "Times New Roman", Times, serif;
+}
+
+
+.topNaviagationLink a
+{
+text-decoration:none;
+color:#000000;
+}
+
+.topNaviagationLink a:hover, .topNaviagationLink a.active 
+{
+text-align:center;
+border-bottom:none;
+color:#0C61C9;
+display: block;
+width:121px;
+height: 35px;
+line-height: 35px;
+background-image:url(http://antipropaganda.net/en/img/tab.png);
+
+}
+
+#mainPicture
+{
+width:670px;
+height:345px;
+background-color:#FFFFFF;
+}
+
+#mainPicture
+{
+width:670px;
+height:345px;
+background-color:#FFFFFF;
+}
+
+#mainPicture .picture
+{
+position:relative;
+width:650px;
+height:325px;
+top:10px;
+background-repeat:no-repeat;
+margin-left:10px;
+}
+
+#headerTitle
+{
+position:relative;
+top:30px;
+left:425px;
+font-size:25px;
+color:#FFFFFF;
+font-family:"Adobe Garamond Pro Bold", Georgia, "Times New Roman", Times, serif;
+}
+
+#headerSubtext
+{
+position:relative;
+top:30px;
+left:425px;
+font-size:14px;
+color:#c6c6c6;
+font-family:"Adobe Garamond Pro Bold", Georgia, "Times New Roman", Times, serif;
+}
+
+.contentBox
+{
+width:670px;
+height:auto;
+background-color:#FFFFFF;
+margin-top:10px;
+}
+
+.contentBox .innerBox
+{
+position:relative;
+width:650px;
+height:auto;
+top:10px;
+margin-left:10px;
+background-image:url(http://antipropaganda.net/en/img/content_back.png);
+background-repeat:repeat-x;
+padding-bottom:35px;
+}
+
+.contentTitle
+{
+font-size:19px;
+font-family:'Georgia', 'Garamond';
+margin-bottom:0px;
+padding-top:18px;
+margin-left:15px;
+margin-top:15px;
+text-decoration:none;
+color:#000000;
+}
+
+
+.contentTitle a
+{
+text-decoration:none;
+color:#000000;
+font-face:Georgia;
+}
+
+.contentText
+{
+font-size:13px;
+line-height:24px;
+margin-left:13px;
+margin-right:13px;
+text-decoration:none;
+color:#000000;
+}
+
+#footer {
+
+width: 670px;
+height:16px;
+background: url(http://antipropaganda.net/en/img/footer.png) no-repeat;
+text-align:center;
+font-size:9px;
+font-family:Arial, Helvetica, sans-serif;
+color:#386172;
+padding-top:5px;
+}
+
+#footer a
+{
+text-decoration:none;
+font-size:9px;
+color:#386172;
+}
+
+html, body {
+text-align: center;
+}
+p {text-align: left;}
+
+.path {
+padding: 10px 20px 0 10px;
+margin-left:5px;
+}
+
+.path a{
+color:#386172;
+}
+
+.more {
+float:right;
+display:inline;
+margin-right:25px;
+}
+
+.more a{
+color:#386172;
+text-decoration:none;
+}
+
+.previous {
+padding:0;
+margin:0;
+width:125px;
+float:left;	
+}
+
+.countnews {
+padding:0;
+margin:0;
+width:365px;
+float:left;
+text-align:center;
+color:#ed960e;	
+}  
+
+.paginationnews {
+padding:5px 0 0 10px;
+margin:0;
+margin-bottom:20px;
+*margin-bottom:120px;
+width:640px;
+float:left;
+border:1px  dotted #000033;
+border-left:none;
+border-right:none;
+font-size:17px;
+text-align:center;
+clear:both;
+}
+
+.paginationnews a{
+color:#386172;
+text-decoration:none; 
+}
+
+.paginationnews a, .paginationnews b {
+	padding:0 5px; 
+  background:none;
+}
+
+
+.paginationnews a img {
+vertical-align:middle;
+padding-left:10px;
+border:none;
+background:none;
+}
+
+
+.next {
+padding:0;
+margin:0;
+width:125px;
+float:left;
+text-align:right;
+}
+
+.previous a:hover img {
+border:none;
+background:none;
+}
+
+.next a:hover img {
+border:none;
+background:none; 
+}
+
